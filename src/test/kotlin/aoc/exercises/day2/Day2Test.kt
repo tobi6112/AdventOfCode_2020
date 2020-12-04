57c92b48433431546b1918412656713c8780e896
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2020-12-04 17:23:46 +0100

    fix tests

--- src/test/kotlin/aoc/exercises/day2/Day2Test.kt
+++ src/test/kotlin/aoc/exercises/day2/Day2Test.kt
@@ -1,7 +1,6 @@
 package aoc.exercises.day2
 
 import aoc.base.AbstractExerciseTest
-import aoc.exercises.Day2
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
